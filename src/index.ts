3e929b41ac8261783d989bb343c27fc15b1ca6ca
Vikram Peddinti <vikram.peddinti@gmail.com>
2024-08-25 21:01:12 -0700

    added messy input handling, simple polling in the ticker loop for smooth movement. Parse out to finer classes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+// stores the keydown and keyup events for easy access within the game loop
+class InputHandler {
+    private keysPressed: Set<string> = new Set();
+
+    constructor() {
+        window.addEventListener('keydown', (e: KeyboardEvent) => {
+            this.keysPressed.add(e.code);
+        });
+        window.addEventListener('keyup', (e: KeyboardEvent) => {
+            this.keysPressed.delete(e.code);
+        });
+    }
+
+    isKeyPressed(key: string): boolean {
+        return this.keysPressed.has(key);
+    }
+}
+
+
 import * as PIXI from 'pixi.js';
 
 // application class creates a renderer, creates a stage & starts the ticker
@@ -6,6 +25,8 @@ const app = new PIXI.Application();
 // async function to initialize the app
 async function initializeApp() {
     await app.init({ width: 960, height: 520 });
+
+    const inputHandler = new InputHandler();
 
     // add the canvas to the DOM
     document.body.appendChild(app.canvas);
@@ -20,7 +41,22 @@ async function initializeApp() {
     let elapsed = 0.0;
     app.ticker.add((ticker) => {
         elapsed += ticker.deltaTime;
-        graphicsRect.x = 100.0 + Math.sin(elapsed / 20) * 100.0;
+        
+        // poll the input
+        if(inputHandler.isKeyPressed('ArrowDown')){
+            graphicsRect.position.y += 5;
+        }
+        if(inputHandler.isKeyPressed('ArrowUp')){
+            graphicsRect.position.y -= 5;
+        }
+        if(inputHandler.isKeyPressed('ArrowRight')){
+            graphicsRect.position.x += 5;
+        }
+        if(inputHandler.isKeyPressed('ArrowLeft')){
+            graphicsRect.position.x -= 5;
+        }
+
+        //graphicsRect.x = 100.0 + Math.sin(elapsed / 20) * 100.0;
     });
 }
 
