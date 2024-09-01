ec535846790eedd86c5453ed6aba0ae37881ef4f
Vikram Peddinti <vikram.peddinti@gmail.com>
2024-08-31 20:32:32 -0700

    Adding a github action for running the sample tests in all branches on push

--- src/collision.test.ts
+++ src/collision.test.ts
@@ -1,3 +1,6 @@
+
+// this class tests the basic AABB narrow phase collision logic between two entities
+// instead of mocking entities, we're just testing if the points are bounded
 describe('Collision Test', () => {
     it('should pass', () => {
       expect(true).toBe(true);
